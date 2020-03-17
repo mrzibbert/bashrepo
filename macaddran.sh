a91ed1a7c7f1ecc9fe7f44a983e7f60285bc7dc8
mrzibbert <mrzibbert@eclipso.eu>
2020-03-17 12:52:53 +0300

    Add bash script random mac address in Debian 10 buster

---
+++ macaddran.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if ip link show enp3s0 | grep -q "state UP"; 
+then
+    echo "Enp3s0 now is Online and going to shutdown Net.Interface enp3s0"
+    ip link set dev enp3s0 down 
+    sleep 5
+    MAC=$((date; cat /proc/interrupts) | md5sum | sed -r 's/^(.{12}).*$/\1/; s/([0-9a-f]{2})/\1:/g; s/:$//;')
+    echo $MAC
+    ip link set dev enp3s0 address $MAC
+    sleep 2
+    echo "Show new MAC address"
+    ip link show enp3s0
+    sleep 1
+    ip link set dev enp3s0 up
+    sleep 5
+    systemctl restart NetworkManager.service
+    sleep 2
+    systemctl restart openvpn@*.service
+else
+    echo "Net.Interface not online"
+    echo "Restart NetworkManager.service"
+    systemctl restart NetworkManager.service
+fi
+
